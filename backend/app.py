b34a0652dfdb9e4c473006f1baa56c0919cd5c32
ohashmi21 <68452122+ohashmi21@users.noreply.github.com>
2024-08-15 13:17:09 -0400

    fixed python code

--- backend/app.py
+++ backend/app.py
@@ -5,28 +5,6 @@ from docx import Document
 import genanki
 import re
 
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    dname = request.form['dname']
-    file = request.files['notes']
-    
-    # Process the file and create the Anki deck
-    deck = process_request(dname, file)
-    
-    # Save the deck to a file
-    output_file = f"{dname}.apkg"
-    save_deck(deck, output_file)
-    response = send_file(output_file, as_attachment=True, download_name=output_file)
-    os.remove(f"{dname}.apkg")
-    return response
-
-if __name__ == '__main__':
-    app.run(port=3000)
-
 def process_request(dname, file):
     doc = read_document(file)
     def_card, cloze_card = extract_cards(doc)
@@ -142,3 +120,26 @@ def create_anki_deck(definition_cards, cloze_deletion_cards, deck_name="My Deck"
 def save_deck(deck, file_name='output.apkg'):
     # Save the deck as an .apkg file
     genanki.Package(deck).write_to_file(file_name)
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    dname = request.form['dname']
+    file = request.files['notes']
+    
+    # Process the file and create the Anki deck
+    deck = process_request(dname, file)
+    
+    # Save the deck to a file
+    output_file = f"{dname}.apkg"
+    save_deck(deck, output_file)
+    response = send_file(output_file, as_attachment=True, download_name=output_file)
+    os.remove(f"{dname}.apkg")
+    return response
+
+if __name__ == '__main__':
+    app.run(port=3000)
+
+
